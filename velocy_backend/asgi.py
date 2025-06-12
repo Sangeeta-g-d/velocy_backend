99b9d850a343110b41d2a154f10c974990280c7b
Sangeeta-g <sangeetadani599@gmail.com>
2025-06-12 11:22:49 +0530

    asgi file

--- velocy_backend/asgi.py
+++ velocy_backend/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import rider_part.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'velocy_verse.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
