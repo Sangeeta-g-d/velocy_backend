cf72c6e73e8c7719654e1b804714d8089827ee39
Sangeeta-g <sangeetadani599@gmail.com>
2025-07-15 15:27:22 +0530

    check websocket

--- rider_part/routing.py
+++ rider_part/routing.py
@@ -3,7 +3,7 @@ from .consumers import RideTrackingConsumer,RideNotificationConsumer,ChatConsume
 
 websocket_urlpatterns = [
     re_path(r'ws/ride-tracking/(?P<ride_id>\d+)/$', RideTrackingConsumer.as_asgi()),
-    re_path(r'ws/rider/otp/$', RideNotificationConsumer.as_asgi()),
+    re_path(r'ws/rider/notifications/$', RideNotificationConsumer.as_asgi()),
     re_path(r'ws/chat/(?P<ride_id>\w+)/$', ChatConsumer.as_asgi()),
     re_path(r'ws/payment/status/(?P<ride_id>\d+)/$', RidePaymentStatusConsumer.as_asgi()),
 ]
