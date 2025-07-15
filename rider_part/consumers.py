7217951659a5a0c006a7c70c5efe27822a0cf114
Sangeeta-g <sangeetadani599@gmail.com>
2025-07-15 15:26:14 +0530

    removed auth

--- rider_part/consumers.py
+++ rider_part/consumers.py
@@ -45,12 +45,11 @@ class RideTrackingConsumer(AsyncWebsocketConsumer):
 class RideNotificationConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.user = self.scope["user"]
-        if self.user.is_authenticated:
-            self.group_name = f"user_{self.user.id}"
-            await self.channel_layer.group_add(self.group_name, self.channel_name)
-            await self.accept()
-        else:
-            await self.close()
+        
+        self.group_name = f"user_{self.user.id}"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
+      
 
     async def disconnect(self, close_code):
         if hasattr(self, 'group_name'):
