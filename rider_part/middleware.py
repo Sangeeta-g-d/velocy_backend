85fbe0c5f3463749b875e9eaad5dcc234e28d7e5
Sangeeta-g <sangeetadani599@gmail.com>
2025-07-15 17:14:03 +0530

    middleware

--- rider_part/middleware.py
+++ rider_part/middleware.py
@@ -9,18 +9,18 @@ from rest_framework_simplejwt.tokens import AccessToken
 User = get_user_model()
 
 class JWTAuthMiddleware:
-    """
-    ASGI Middleware for authenticating WebSocket connections using JWT.
-    """
-
     def __init__(self, app):
         self.app = app
 
     async def __call__(self, scope, receive, send):
+        path = scope.get("path", "")
+        if path == "/ws/rider/otp/":
+            scope['user'] = AnonymousUser()
+            return await self.app(scope, receive, send)
+
         query_string = scope.get("query_string", b"").decode()
         query_params = parse_qs(query_string)
         token = query_params.get("token", [None])[0]
-
         scope['user'] = await self.get_user(token)
         return await self.app(scope, receive, send)
 
@@ -28,7 +28,8 @@ class JWTAuthMiddleware:
     def get_user(self, token):
         try:
             access_token = AccessToken(token)
-            user_id = access_token['user_id']
+            user_id = access_token.get("user_id")
             return User.objects.get(id=user_id)
-        except Exception:
+        except Exception as e:
+            print("JWT error:", e)
             return AnonymousUser()
