2b09c8a7e306bb992461f6517742290353237426
Sangeeta-g <sangeetadani599@gmail.com>
2025-08-19 16:43:25 +0530

    added referral code

--- auth_api/utils/otp.py
+++ auth_api/utils/otp.py
@@ -1,5 +1,3 @@
-# utils/otp.py
-
 from twilio.rest import Client
 import random
 from django.conf import settings
@@ -8,19 +6,17 @@ def generate_otp():
     return str(random.randint(100000, 999999))
 
 def send_otp(phone_number, otp):
-    from twilio.rest import Client
-    from django.conf import settings
-
     print(f"Using TWILIO_PHONE_NUMBER: {settings.TWILIO_PHONE_NUMBER}")
 
-    # ✨ Custom formatted message
     message_body = (
         f"Velocy Verse OTP: {otp}. "
         "Do not share this code with anyone. "
         "This code is valid for 2 minutes."
     )
 
+    # Authenticate using API Key SID + Secret + Account SID
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
     message = client.messages.create(
         body=message_body,
         from_=settings.TWILIO_PHONE_NUMBER,
